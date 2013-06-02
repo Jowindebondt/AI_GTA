f241d34703d1f911a6237c88d99248ef0018403b
Carlo Bril <bril_carlo@hotmail.com>
2013-06-02 16:47:05 +0200

    Added ProcessSubgoals dont think its correct with the datastructure Stack

--- GTA/GTA/GTA/GoalDriven/CompositeGoal.cs
+++ GTA/GTA/GTA/GoalDriven/CompositeGoal.cs
@@ -7,7 +7,7 @@ namespace GTA
 {
     class CompositeGoal : Goal
     {
-        public List<Goal> SubGoals { get; set; }
+        public Stack<Goal> SubGoals { get; set; }
 
         public override void Activate()
         {
@@ -26,17 +26,40 @@ namespace GTA
 
         public void AddSubgoal(Goal _goal)
         {
-            SubGoals.Add(_goal);
+            SubGoals.Push(_goal);
         }
 
         public void RemoveAllSubgoals()
         {
+            foreach (var subGoal in SubGoals)
+            {
+                subGoal.Terminate();
+            }
             SubGoals.Clear();
         }
 
         public Status ProcessSubgoals()
         {
-            return 0;
+            while (SubGoals.Count != 0 && (SubGoals.First().StatusOfGoal == Status.Completed || SubGoals.First().StatusOfGoal == Status.Failed)) // remove all completed and failed goals from the front of the subgoal list
+            {
+                SubGoals.First().Terminate();
+                SubGoals.Pop();
+            }
+
+            if (SubGoals.Count != 0)
+            {
+                Status StatusOfSubGoals = SubGoals.First().Process();
+                if (StatusOfSubGoals == Status.Completed && SubGoals.Count > 1)
+                {
+                    return Status.Active;
+                }
+
+                return StatusOfSubGoals;
+            }
+            else
+            {
+                return Status.Completed;
+            }
         }
     }
 }
